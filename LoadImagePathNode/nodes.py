6a6ee3d13284e426efd03201105e796e7c7e8453
za-wa-n-go <mszhrk@gmail.com>
2024-06-29 11:17:04 +0900

    URLからの読み込みも可能にした

--- LoadImagePathNode/nodes.py
+++ LoadImagePathNode/nodes.py
@@ -14,8 +14,7 @@ from comfy_extras.nodes_mask import ImageCompositeMasked
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "comfy"))
 sys.path.append('../ComfyUI')
 
-class ZwngLoadImagePath:
-
+class ZwngLoadImagePathOrURL:
     def __init__(self):
         self.input_dir = os.path.join(os.getcwd()+'/ComfyUI', "input")
         
@@ -24,9 +23,7 @@ class ZwngLoadImagePath:
         return {"required":
                     {"image_path": ("STRING", {"default": '"./input/example.png"', "multiline": False}),}
                 }
-
     CATEGORY = "ZWNG"
-
     RETURN_TYPES = ("IMAGE", "MASK")
     FUNCTION = "load_image"
     
@@ -34,8 +31,7 @@ class ZwngLoadImagePath:
         image_path = re.sub(r'^"(.*)"$', r'\1', image_path)
             
         if image_path.startswith('http'):
-            from io import BytesIO
-            i = self.download_image(image_path)
+            i = self.load_image_from_url(image_path)
         else:
             try:
                 i = Image.open(image_path).convert("RGBA")
@@ -45,33 +41,25 @@ class ZwngLoadImagePath:
                 
         image_np = np.array(i).astype(np.float32) / 255.0
         image_tensor = torch.from_numpy(image_np)[None,]
-
         out_images = [img[:,:,:3] for img in image_tensor]
-        out_alphas = [img[:,:,3] if img.shape[2] > 3 else torch.ones_like(img[:,:,0]) for img in image_tensor]
+        out_alphas = [img[:,:,3] if img.shape[2] > 3 else None for img in image_tensor]
         result_image = torch.stack(out_images)
-        result_mask = torch.stack(out_alphas)  # ここではアルファチャンネルをそのまま使用
+        result_mask = torch.stack(out_alphas) if out_alphas[0] is not None else None
         
         return (result_image, result_mask)
 
-    def download_image(self, url):
+    def load_image_from_url(self, url):
         try:
             response = requests.get(url)
             response.raise_for_status()
-            img = Image.open(BytesIO(response.content)).convert("RGBA")
-            return img
-        except requests.exceptions.HTTPError as errh:
-            print(f"\033[34mWAS NS\033[0m Error: HTTP Error: ({url}): {errh}") 
-        except requests.exceptions.ConnectionError as errc:
-            print(f"\033[34mWAS NS\033[0m Error: Connection Error: ({url}): {errc}")  
-        except requests.exceptions.Timeout as errt:
-            print(f"\033[34mWAS NS\033[0m Error: Timeout Error: ({url}): {errt}")
+            return Image.open(BytesIO(response.content)).convert("RGBA")
         except requests.exceptions.RequestException as err:
-            print(f"\033[34mWAS NS\033[0m Error: Request Exception: ({url}): {err}")
+            print(f"\033[34mWAS NS\033[0m Error: Failed to load image from URL: ({url}): {err}")
+            return Image.new(mode='RGBA', size=(512, 512), color=(0, 0, 0, 0))
         
     @classmethod
     def IS_CHANGED(s, image_path):
         image_path = re.sub(r'^"(.*)"$', r'\1', image_path)
-
         if image_path.startswith('http'):
             return True
         m = hashlib.sha256()
@@ -80,8 +68,8 @@ class ZwngLoadImagePath:
         return m.digest().hex()
 
 NODE_CLASS_MAPPINGS = {
-    'ZwngLoadImagePath': ZwngLoadImagePath,
+    'ZwngLoadImagePathOrURL': ZwngLoadImagePathOrURL,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
-    'ZwngLoadImagePath': 'Load Image Path',
+    'ZwngLoadImagePathOrURL': 'Load Image Path or URL',
 }
